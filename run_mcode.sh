0a9d1fa1f087e3ff4fe863dc6a49d9c20c27ff28
Benjamin Gutierrez <ben.gutierrez@forallsecure.com>
2024-05-01 14:56:22 -0400

    mcode

--- run_mcode.sh
+++ run_mcode.sh
@@ -1,12 +1,16 @@
 
 MAYHEM_DOCKER_REGISTRY=$(mayhem docker-registry)
+DURATION=180
 
 docker build -t $MAYHEM_DOCKER_REGISTRY/bengutierrez/iotgoat-mayhem -f DockerfileMayhem .
 docker push $MAYHEM_DOCKER_REGISTRY/bengutierrez/iotgoat-mayhem
 
-#mayhem run --duration 600 -f mayhemfiles/patch-cmdline.mayhemfile .
-#mayhem run --duration 600 -f mayhemfiles/patch-dtb.mayhemfile .
-mayhem run --duration 600 -f mayhemfiles/sstrip.mayhemfile .
-mayhem run --duration 600 -f mayhemfiles/trx2edips.mayhemfile .
-mayhem run --duration 600 -f mayhemfiles/zyxbcm.mayhemfile .
-mayhem run --duration 600 -f mayhemfiles/zyimage.mayhemfile .
+#mayhem run --duration $DURATION -f mayhemfiles/patch-cmdline.mayhemfile .
+#mayhem run --duration $DURATION -f mayhemfiles/patch-dtb.mayhemfile .
+mayhem run --duration $DURATION -f mayhemfiles/spw303v.mayhemfile .
+mayhem run --duration $DURATION -f mayhemfiles/sstrip.mayhemfile .
+mayhem run --duration $DURATION -f mayhemfiles/trx.mayhemfile .
+mayhem run --duration $DURATION -f mayhemfiles/trx2edips.mayhemfile .
+mayhem run --duration $DURATION -f mayhemfiles/xorimage.mayhemfile .
+mayhem run --duration $DURATION -f mayhemfiles/zyxbcm.mayhemfile .
+mayhem run --duration $DURATION -f mayhemfiles/zyimage.mayhemfile .
