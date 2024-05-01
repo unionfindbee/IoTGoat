327d74234b8e46c3abec8472ea5cae0181d37356
Benjamin Gutierrez <ben.gutierrez@forallsecure.com>
2024-05-01 14:31:47 -0400

    mcode

--- run_mcode.sh
+++ run_mcode.sh
@@ -4,7 +4,9 @@ MAYHEM_DOCKER_REGISTRY=$(mayhem docker-registry)
 docker build -t $MAYHEM_DOCKER_REGISTRY/bengutierrez/iotgoat-mayhem -f DockerfileMayhem .
 docker push $MAYHEM_DOCKER_REGISTRY/bengutierrez/iotgoat-mayhem
 
-#mayhem run -f mayhemfiles/patch-cmdline.mayhemfile .
-#mayhem run -f mayhemfiles/patch-dtb.mayhemfile .
-mayhem run -f mayhemfiles/sstrip.mayhemfile .
-mayhem run -f mayhemfiles/zyxbc.mayhemfile .
+#mayhem run --duration 600 -f mayhemfiles/patch-cmdline.mayhemfile .
+#mayhem run --duration 600 -f mayhemfiles/patch-dtb.mayhemfile .
+mayhem run --duration 600 -f mayhemfiles/sstrip.mayhemfile .
+mayhem run --duration 600 -f mayhemfiles/trx2edips.mayhemfile .
+mayhem run --duration 600 -f mayhemfiles/zyxbcm.mayhemfile .
+mayhem run --duration 600 -f mayhemfiles/zyimage.mayhemfile .
