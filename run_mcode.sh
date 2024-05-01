45ea7a1167fe53300d87d8317a7c651162273556
Benjamin Gutierrez <ben.gutierrez@forallsecure.com>
2024-05-01 11:35:00 -0400

    mcode

--- run_mcode.sh
+++ run_mcode.sh
@@ -2,5 +2,6 @@
 MAYHEM_DOCKER_REGISTRY=$(mayhem docker-registry)
 
 docker build -t $MAYHEM_DOCKER_REGISTRY/bengutierrez/iotgoat-mayhem -f DockerfileMayhem .
+docker push $MAYHEM_DOCKER_REGISTRY/bengutierrez/iotgoat-mayhem
 
 mayhem run -f mayhemfiles/patch-cmdline.mayhemfile .
